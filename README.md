b76d002ba06257bbea1cf6a10993b3ca370a06c9
m.malkin <mmalkin@fun-box.ru>
2020-10-19 17:52:19 +0300

    Version bumpup: 0.1.0 → 0.1.1

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ end
 
 defp deps do
   [
-    {:excoveralls_linter, "~> 0.1.0", only: :test}
+    {:excoveralls_linter, "~> 0.1.1", only: :test}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCoverallsLinter.MixProject do
   def project do
     [
       app: :excoveralls_linter,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
