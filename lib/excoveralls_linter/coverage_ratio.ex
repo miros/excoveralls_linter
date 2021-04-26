35e91b9642384bff8a4d1b96ba53a35ce93e0874
m.malkin <mmalkin@fun-box.ru>
2021-04-26 18:12:25 +0300

    small bugfix for zero coverage cases

--- lib/excoveralls_linter/coverage_ratio.ex
+++ lib/excoveralls_linter/coverage_ratio.ex
@@ -1,5 +1,6 @@
 defmodule ExCoverallsLinter.CoverageRatio do
   @type t :: float
 
-  def round(coverage), do: Float.round(coverage, 3)
+  def round(coverage) when is_integer(coverage), do: coverage
+  def round(coverage) when is_float(coverage), do: Float.round(coverage, 3)
 end
